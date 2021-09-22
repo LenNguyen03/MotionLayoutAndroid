2f3656a48458c31aa9ed07c0db7d9b43a2f7ecac
LenNguyenVan13 <nguyenvanlentech@gmail.com>
2021-09-22 23:23:25 +0700

    Done Giao Dien Am Nhac Work OK !

---
+++ app/src/main/java/com/se/motionlayoutandroid/MusicAppScreen1.java
@@ -0,0 +1,14 @@
+package com.se.motionlayoutandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MusicAppScreen1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_app_screen1);
+    }
+}
---
+++ app/src/main/java/com/se/motionlayoutandroid/MusicAppScreen2.java
@@ -0,0 +1,20 @@
+package com.se.motionlayoutandroid;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+public class MusicAppScreen2 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_music_app_screen2, container, false);
+        return view;
+    }
+}
